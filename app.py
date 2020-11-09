5fb83d0b2c7b62f4843545ad7669777b252295d2
Dan Wright <daniel-g-wright@hotmail.co.uk>
2020-11-09 17:14:58 +0000

    Add generic layout to base.html and begin to add static files.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, redirect
-
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
@@ -11,9 +13,9 @@ def index():
 
 
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', '8000')), debug=True)
+#app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', '8000')), debug=True)
 
-#if __name__ == "__main__":
-#    app.run(host=os.environ.get("IP"),
-#            port=int(os.environ.get("PORT")),
-#            debug=False)
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
