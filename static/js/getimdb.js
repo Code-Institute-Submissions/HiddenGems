240103604c84adc8a394e0cef41e76a1d1d81124
Dan Wright <daniel-g-wright@hotmail.co.uk>
2020-11-13 13:41:34 +0000

    Update movie addition process to include a confirmation form, where python will push data to MongoDB

--- static/js/getimdb.js
+++ static/js/getimdb.js
@@ -40,6 +40,7 @@ function getIMDB(){
                 <p id="movie-actors-${[i]}"class="card-text"></p>
               </div>
               <button id="select-button-${[i]}" onclick="getDetails(this.id)" class="btn-sm btn-primary">This One!</button>
+              <a href="/find_movie" class="btn-sm btn-primary">Go!</a>
             </div>`);
             //Add select button which pulls info into dict.
       clickCounter++;
@@ -63,12 +64,20 @@ function getIMDB(){
 
 
 
-
+// push data into a form and ask to confirm. Then have python pull data. Ask how you would categorise the film?
 
 function getDetails(id){
     switch(id){
         case "select-button-0":
             console.log("1 was pressed");
+            var title = $("#movie-title-0").text();
+            var year = $("#movie-year-0").text();
+            var image = $("#movie-image-0").attr('src');
+            var actors = $("#movie-actors-0").text();
+            console.log(title);
+            console.log(year);
+            console.log(image);
+            console.log(actors);
             break;
         case "select-button-1":
             console.log("2 was pressed");
@@ -90,4 +99,20 @@ function getDetails(id){
             console.log("6 was pressed");
             break;
     }
+  localStorage.setItem("movietitle", title);
+  localStorage.setItem("movieyear", year);
+  localStorage.setItem("movieactors", actors);
+  localStorage.setItem("movieimage", image);
+}
+
+
+function setDetails() {
+  var movietitle = localStorage.getItem("movietitle");
+  $("#movietitle").val(movietitle);
+  var movieyear = localStorage.getItem("movieyear");
+  $("#movieyear").val(movieyear);
+  var movieactors = localStorage.getItem("movieactors");
+  $("#movieactors").val(movieactors);
+  var movieimage = localStorage.getItem("movieimage");
+  $("#movieimage").val(movieimage);
 }
