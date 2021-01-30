5349ad0efc603c7060b0d19113bd1a035f8e1a01
Dan Wright <daniel-g-wright@hotmail.co.uk>
2021-01-30 18:36:21 +0000

    Small styling changes to the add movie button, located in getimdb.js

--- static/js/getimdb.js
+++ static/js/getimdb.js
@@ -38,7 +38,7 @@ function getIMDB(){
                 <h4 id="movie-title-${[i]}" class="card-title"></h4>
                 <p id="movie-year-${[i]}"class="card-text"></p>
                 <p id="movie-actors-${[i]}"class="card-text"></p>
-                <a id="select-button-${[i]}" onclick="return getDetails(this.id)" href="/find_movie" class="btn-sm btn-primary">Go!</a>
+                <a id="select-button-${[i]}" onclick="return getDetails(this.id)" href="/find_movie" class="btn btn-md btn-success btn-block text-uppercase">This One!</a>
               </div>
             </div>`);
             //Add select button which pulls info into dict.
