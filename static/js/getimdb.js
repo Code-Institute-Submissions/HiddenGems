0cee2e58ed6bb89ad88487b5794ffa7d99e1bc6a
Dan Wright <daniel-g-wright@hotmail.co.uk>
2021-02-13 13:26:09 +0000

    Add character counter to review section of find_movie.html

--- static/js/getimdb.js
+++ static/js/getimdb.js
@@ -215,3 +215,13 @@ function getValidation() {
 function goBack() {
   window.history.back();
 }
+
+
+function textCounter(field,cnt, maxlimit) {         
+	var cntfield = document.getElementById(cnt)	
+     if (field.value.length > maxlimit) // if too long...trim it!
+		field.value = field.value.substring(0, maxlimit);
+		// otherwise, update 'characters left' counter
+		else
+		cntfield.value = maxlimit - field.value.length;
+}
